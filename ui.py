4e1abaae937cc33b02c614ea64e2af75ebbb1ffb
Zack Dowd <zdowd2796@gmail.com>
2023-12-14 11:30:26 -0700

    fetching questions and displaying them

--- ui.py
+++ ui.py
@@ -1,9 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
+
 class UserInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(width=600, height=500, padx=20, pady=20, bg=THEME_COLOR)
@@ -12,7 +16,12 @@ class UserInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, text="Question goes here", font=("Arial", 20, "italic"), fill=THEME_COLOR)
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     width=280,
+                                                     text="Question goes here",
+                                                     font=("Arial", 20, "italic"),
+                                                     fill=THEME_COLOR)
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_img = PhotoImage(file="./images/true.png")
@@ -23,5 +32,10 @@ class UserInterface:
         self.false_button = Button(image=false_img, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
