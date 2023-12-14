d85cbc23ce50b8f8e1c67d8209ca4bf97f332952
Zack Dowd <zdowd2796@gmail.com>
2023-12-14 11:59:12 -0700

    quiz finished and working

--- ui.py
+++ ui.py
@@ -12,7 +12,7 @@ class UserInterface:
         self.window.title("Quizzler")
         self.window.config(width=600, height=500, padx=20, pady=20, bg=THEME_COLOR)
 
-        self.score_label = Label(text=f"Score: {self.quiz.score}", fg="white", bg=THEME_COLOR)
+        self.score_label = Label(text=f"Score: 0/10", fg="white", bg=THEME_COLOR)
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
@@ -37,13 +37,27 @@ class UserInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}/10")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz!")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
 
     def correct_answer(self):
-        self.quiz.check_answer("True")
-        self.get_next_question()
+        self.give_feedback(self.quiz.check_answer("True"))
 
     def incorrect_answer(self):
-        self.quiz.check_answer("False")
-        self.get_next_question()
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        elif not is_right:
+            self.canvas.config(bg="red")
+        self.canvas.update()
+        self.window.after(1000, self.get_next_question())
